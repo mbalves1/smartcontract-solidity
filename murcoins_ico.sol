6cdd268b56f777a3bdd1157cdb8013d5fc7f9a67
mbalves1 <mbalves1@outlook.com>
2024-09-03 16:41:37 -0300

    feat: create branch 01

---
+++ murcoins_ico.sol
@@ -0,0 +1,15 @@
+// Murcoins ICO
+// Version of compiler
+pragma solidity ^0.8.26;
+
+contract murcoin_ico {
+    // Introducing the max number of Murcoin available for sale
+    uint public max_murcoins = 1000000;
+
+    // Introducing the USD to Murcoins conversion rate
+    uint public usd_to_murcoins = 1000;
+
+    // Introducing the total number of Murcoin that have been bought by the investors
+    uint public total_murcoins_bought = 0;
+    
+}
